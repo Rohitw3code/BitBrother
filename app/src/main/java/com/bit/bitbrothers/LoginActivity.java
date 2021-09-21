21e1f611f292b2f24484c399add7a857672ab5cd
rohitcode005 <rohitcode005@gmail.com>
2021-09-21 18:00:33 +0530

    added login

--- app/src/main/java/com/bit/bitbrothers/LoginActivity.java
+++ app/src/main/java/com/bit/bitbrothers/LoginActivity.java
@@ -6,19 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class SignUpActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-
-
+        setContentView(R.layout.activity_login);
     }
 
-
-    public void alreadyHaveAccount(View view) {
-        Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+    public void createAccount(View view) {
+        Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
         startActivity(intent);
     }
 }
